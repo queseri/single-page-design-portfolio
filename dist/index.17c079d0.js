9c3ad501927707d41ce85ffcd1a91ada35b8b04b
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-09-06 20:43:18 +0200

    fix: setup parcel for deployment

---
+++ dist/index.17c079d0.js
@@ -0,0 +1,2 @@
+const slides=document.querySelectorAll(".btn-carousel");function currentSlide(e){slides.forEach((e=>e.classList.remove("active-btn"))),e.target.classList.add("active-btn")}slides.forEach((e=>e.addEventListener("click",currentSlide)));
+//# sourceMappingURL=index.17c079d0.js.map
